fad9590bd9ffd9bc62160e77d1032445a7f2c220
enzoptortelli <58557898+enzoptortelli@users.noreply.github.com>
2020-07-21 17:41:24 -0300

    Create formatador-cpf.js

---
+++ formatador-cpf.js
@@ -0,0 +1,10 @@
+// você deve mudar #NOME para o nome da <input> do cpf em seu site
+let cpfInput = document.querySelector('#NOME');
+
+cpfInput.addEventListener('input', event => {
+  if(event.inputType !== 'deleteContentBackward') { //caso esse if não estivesse aqui, o usuário não iria conseguir apagar os pontos e o traço.
+    if(cpfInput.value.length == 3) cpfInput.value += '.';
+    if(cpfInput.value.length == 7) cpfInput.value += '.';
+    if(cpfInput.value.length == 11) cpfInput.value += '-';
+  }
+});
