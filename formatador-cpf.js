7f5405db7567a148f1d2011d269619459e77f55e
enzoptortelli <58557898+enzoptortelli@users.noreply.github.com>
2022-08-19 15:13:36 -0300

    Removi a variável 'cpfInput'

--- formatador-cpf.js
+++ formatador-cpf.js
@@ -1,7 +1,6 @@
 // você deve mudar #NOME para que a função querySelector selecione a <input> do cpf em seu site.
-let cpfInput = document.querySelector('#NOME');
 
-cpfInput.addEventListener('input', event => {
+document.querySelector('#NOME').addEventListener('input', event => {
   if(event.inputType !== 'deleteContentBackward') { //caso esse if não estivesse aqui, o usuário não iria conseguir apagar os pontos e o traço.
     if(cpfInput.value.length == 3) cpfInput.value += '.';
     if(cpfInput.value.length == 7) cpfInput.value += '.';
